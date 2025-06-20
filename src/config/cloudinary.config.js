7033ff34a9bc4f033bd45c249b596a06b056066a
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2025-06-20 19:42:25 +0530

    feat: Professional Grade Setup First Steps Completed

---
+++ src/config/cloudinary.config.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary";
+
+import fs from "fs";
+
+// Configuuration for cloudinary that gives us permission to upload the files
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    //Check: If file path exists of not
+    if (!localFilePath) return null;
+
+    // Upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // File has been successfully uploaded
+    console.log("File is uploaded on cloudinary!", response.url);
+
+    return response;
+  } catch (error) {
+    // Just remove any file from local server first, to remove any malicious file that may be harmful
+
+    fs.unlinkSync(localFilePath); // Removes the locally saved temporary file as the upload operation failed
+
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
